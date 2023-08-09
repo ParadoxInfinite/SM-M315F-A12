fcbaccc0885481e6a370777d349fe8bf0fff2395
Samarth Kulkarni <10831629+ParadoxInfinite@users.noreply.github.com>
2023-08-09 20:11:18 +0530

    fix: gitignore and build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,11 @@ export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 
 # toolchain
-sudo echo "Starting build script!"
+sudo echo """
+======================
+Starting build script!
+======================
+"""
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 toolchain
 cd toolchain
 git checkout android-12.1.0_r27
@@ -13,9 +17,16 @@ cd ..
 
 # clang
 wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/1c1069109f294e9ffbdc1ff8541394ab4b5d941d/clang-r416183b1.tar.gz
-mkdir clang && tar xzvf clang-r416183b1.tar.gz -C ./clang
+mkdir clang && tar xzvf clang-r416183b1.tar.gz /dev/null -C ./clang
 
 # build
-sudo chown $(whoami) *
+echo "Setting ownership of files for building..."
+sudo chown $(whoami) * > /dev/null
+echo "Done setting ownership of files."
 make ARCH=arm64 CONFIG_SECTION_MISMATCH_WARN_ONLY=y exynos9610-m31nsxx_defconfig
-make ARCH=arm64 CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -j16 ARCH=arm64 CONFIG_SECTION_MISMATCH_WARN_ONLY=y 
+echo """
+======================
+Build completed!
+======================
+"""
